e03613c49058133aa35697d071b8d5c2ec69bbc6
Rayesunnisha Rayesunnisha <rayesunnisha.rayesunnisha@ibm.com>
2021-04-22 11:00:18 +0530

    Altered abc xyz and created AtoZ

--- AtoZ.java
+++ AtoZ.java
@@ -1,4 +1,4 @@
-public class xyz
+public AtoZ
 {
 	public void display()
 	{
--- abc.java
+++ abc.java
@@ -3,6 +3,8 @@ public class abc
 	public static void main(String args[])
 	{
 		for(int i=0;i<11;i++)
-		System.out.println("Abc project");
+			System.out.println("Abc project");
+	System.out.println("Thank You");	
 }
+
 }
